bf1b33e8f60afbc373225346851e048968303474
smoklien <localnext07@gmail.com>
2024-02-10 16:49:20 +0200

    Source code for lab-1

--- src/lab-1/main.asm
+++ src/lab-1/main.asm
@@ -1,1 +1,43 @@
-; PLAIN TEXT
+STSEG SEGMENT PARA STACK "STACK"
+    DB 64 DUP ( "STACK" )
+STSEG ENDS
+
+DSEG SEGMENT PARA PUBLIC "DATA"
+    SOURCE DB 10, 20, 30, 40
+    DEST DB 4 DUP ( "?" )
+DSEG ENDS
+
+CSEG SEGMENT PARA PUBLIC "CODE"
+    MAIN PROC FAR
+        ASSUME CS: CSEG, DS: DSEG, SS: STSEG
+
+        ; ?????? ??????????
+        PUSH DS
+        MOV AX, 0 ; ??? XOR AX, AX
+        PUSH AX
+
+        ; ????????????? DS
+        MOV AX, DSEG
+        MOV DS, AX
+
+        ; ????????? ?????
+        MOV DEST, 0
+        MOV DEST+1, 0
+        MOV DEST+2, 0
+        MOV DEST+3, 0
+
+        ; ???????????
+        MOV AL, SOURCE
+        MOV DEST+3, AL
+        MOV AL, SOURCE+1
+        MOV DEST+2, AL
+        MOV AL, SOURCE+2
+        MOV DEST+1, AL
+        MOV AL, SOURCE+3
+        MOV DEST, AL
+
+        RET
+    MAIN ENDP
+CSEG ENDS
+    END MAIN
+    
